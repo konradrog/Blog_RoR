110c3227e1dfc13f558107227b82bac06236d8ab
konradrog <konrad1210@gmail.com>
2017-09-04 15:19:30 +0200

    add login/out funcionality - excercise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_admin
+  
+  private
+
+  def current_admin
+    if session[:admin_id]
+      @current_admin ||= Admin.find(session[:admin_id])
+    else
+      nil
+    end
+  end
+
 end
